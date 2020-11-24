68b5b7cc4eb25cb939198f017c3fe921a4dc0b18
dejiii <adeyigaabdulazeez@gmail.com>
2020-11-24 13:05:53 -0800

    Details page completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,29 +10,34 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("clicked!")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
-          </div>
-          <button
-            className="cart-btn"
-            disabled={inCart ? true : false}
-            onClick={() => {
-              console.log("added to cart");
-            }}
-          >
-            {inCart ? (
-              <p className="text-capitalize mb-0" disabled>
-                In cart
-              </p>
-            ) : (
-              <p>Add to cart</p>
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => {
+                    value.addToCart(id);
+                  }}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      In cart
+                    </p>
+                  ) : (
+                    <p>Add to cart</p>
+                  )}
+                </button>
+              </div>
             )}
-          </button>
+          </ProductConsumer>
+
           <div className="card-footer justify-content-between d-flex">
             <p className="align-self-center mb-0">{title}</p>
             <h5 className="text-blue italic mb-0">
