7f90a72d34f411c9c2fd61db47e0508ca60eaa2a
dejiii <adeyigaabdulazeez@gmail.com>
2020-11-24 12:19:47 -0800

    Setting up Product detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,62 @@
 import React, { Component } from "react";
-
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
 export default class Details extends Component {
   render() {
-    return <div>Hello from details</div>;
+    return (
+      <ProductConsumer>
+        {(value) => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart,
+          } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>Model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made by: <span className="text-uppercase">{company}</span>
+                    <strong>
+                      price: <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    Some info about the product
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer>Back to product</ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart(id);
+                      }}
+                    >
+                      {inCart ? "inCart" : "Add to cart"}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
   }
 }
--- src/context.js
+++ src/context.js
@@ -8,16 +8,31 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct,
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
   handleDetail = () => {
     console.log("Hello from detail");
   };
 
-  addToCart = () => {
+  addToCart = (id) => {
     console.log("Hello from add to cart");
   };
+
   render() {
     return (
       <ProductContext.Provider
