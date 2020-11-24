33d603244c456ee90e1ff0f8a0dc2c1dc9b2a377
dejiii <adeyigaabdulazeez@gmail.com>
2020-11-23 19:48:51 -0800

    Product display done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,80 @@
 import React, { Component } from "react";
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>Hello from Product</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("clicked!")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+          </div>
+          <button
+            className="cart-btn"
+            disabled={inCart ? true : false}
+            onClick={() => {
+              console.log("added to cart");
+            }}
+          >
+            {inCart ? (
+              <p className="text-capitalize mb-0" disabled>
+                In cart
+              </p>
+            ) : (
+              <p>Add to cart</p>
+            )}
+          </button>
+          <div className="card-footer justify-content-between d-flex">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue italic mb-0">
+              <span className="mr-1">$</span> {price}
+            </h5>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: #ddd;
+    }
+  }
+
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card-img-top {
+    transition: all 1s linear;
+  }
+
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+`;
